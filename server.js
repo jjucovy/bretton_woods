75d4fdcf1ac2f1136d735c3f84bb2213ab3dc0a4
jjucovy <33159590+jjucovy@users.noreply.github.com>
2026-01-23 08:52:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3685,7 +3685,9 @@ console.log(`   🎯 END OF submitPolicy - about to broadcast`);
 broadcastToRoom(roomId);
 console.log(`   ✅ broadcastToRoom completed`);
 saveState();
-  }); // <-- ADD THIS LINE to close the 'submitPolicy' socket.on handler
+  }); 
+  
+  // <-- ADD THIS LINE to close the 'submitPolicy' socket.on handler
   
   // PLAYER: Deploy troops
   socket.on('deployTroops', ({ roomId, playerId, deployment }) => {
