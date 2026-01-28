489163000f3b5f66d7b5ff5ead9688a95a5d238f
jjucovy <33159590+jjucovy@users.noreply.github.com>
2026-01-28 15:08:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -353,7 +353,7 @@ app.post('/api/import-state/:roomId', express.json({ limit: '10mb' }), (req, res
   let isSuperAdmin = false;
   
   try {
-    async const dbUsers = await queryDatabase('getAllUsers', {});
+     const dbUsers =async await queryDatabase('getAllUsers', {});
     
     if (dbUsers && Array.isArray(dbUsers)) {
       const dbUser = dbUsers.find(u => u.user_id === playerId);
