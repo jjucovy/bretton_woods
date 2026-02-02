c3a2300375244eaf590d087d4ab60d066e922b53
jjucovy <33159590+jjucovy@users.noreply.github.com>
2026-02-02 09:59:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-x// server-multiroom.js - Bretton Woods Multi-Room Server
+// server-multiroom.js - Bretton Woods Multi-Room Server
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
