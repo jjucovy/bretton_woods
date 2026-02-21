8bbb316400cee5689dd2b713886416ca95d76561
jjucovy <33159590+jjucovy@users.noreply.github.com>
2026-02-21 16:42:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5399,6 +5399,7 @@ const countryId = countryData?.country_id || null;
             (roomState && roomState.gamePhase === 'complete');
 
           if (!isCompleted) {
+            debugger
             console.log("game.game_id")
             myActiveGames.push({
               game_id: game.game_id,
